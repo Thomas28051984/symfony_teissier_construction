614a1c11a7abb3a3e96cc423b24563b02de4589d
TEISSIER Thomas <tigerhead28@hotmail.com>
2024-02-26 21:18:31 +0100

    MAJ

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -21,7 +21,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $email = null;
 
     #[ORM\Column]
-    private array $roles = [];
+    private array $roles = [1];
 
     /**
      * @var string The hashed password
